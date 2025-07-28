2c1cbe74297b95c2100e2e0dae8b27d05cdf852e
rizaldiabyannata <aldizar255@gmail.com>
2025-07-29 05:54:02 +0800

    feat: Add secure cookie handling for access and refresh tokens in user login

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,6 +56,13 @@ const loginUser = async (req, res) => {
 
     logger.info(`Admin logged in: ${name}`);
 
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",

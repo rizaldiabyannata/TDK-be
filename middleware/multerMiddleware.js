829404d848b0d29a72707b0a84b819fbf1fc68de
Hazim <hazimmaulana2@gmail.com>
2025-09-12 22:00:54 +0800

    for pulling

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -15,7 +15,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 10 * 1024 * 1024 },
+  // limits: { fileSize: 10 * 1024 * 1024 },
 });
 
 /**
